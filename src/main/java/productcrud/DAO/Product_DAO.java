2ebd437919ab70c7219c1e3812b62c999112573e
KIRAN CHAUDHARI <chaudharikiran1997@gmail.com>
2023-01-08 19:13:22 +0530

    Add All Files
    Complete Add This MVC Project

---
+++ src/main/java/productcrud/DAO/Product_DAO.java
@@ -0,0 +1,54 @@
+package productcrud.DAO;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+import productcrud.model.Product;
+
+@Component
+public class Product_DAO {
+	
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+
+	// Create
+	@Transactional
+	public void createProduct(Product product) {
+		
+		this.hibernateTemplate.saveOrUpdate(product);
+	}
+	
+	@Transactional
+	public void updateProduct(Product product) {
+		this.hibernateTemplate.update(product);
+	}
+
+	// Get All Product
+	public List<Product> getProducts() {
+		List<Product> product = this.hibernateTemplate.loadAll(Product.class);
+		return product;
+	}
+
+	
+	 //Delete Single Product
+	 
+	  @Transactional 
+	  public void deleteProduct(int pid) 
+	  { 
+		  Product p = this.hibernateTemplate.load(Product.class, pid);
+		  this.hibernateTemplate.delete(p); 
+	  }
+	 
+
+	// Get the Single Product
+	public Product getProduct(int pid) {
+		return this.hibernateTemplate.get(Product.class, pid);
+
+	}
+
+}
